f7faf48c8bbe4bef8bdaf882985f343f3b4f541c
digiaimz <info@digiaimz.com>
2023-03-21 12:59:19 +0500

    Change Icon

--- resources/views/dawa_theme/sidebar/navbar_worker.blade.php
+++ resources/views/dawa_theme/sidebar/navbar_worker.blade.php
@@ -9,7 +9,7 @@
 </li>
 
 
- 
+
 <li class="menu   @yield('broadcastlist_active')">
     <a href="{{route('manage-groups.index')}}"  @yield('broadcastlist_active_aria')   class="dropdown-toggle">
         <div class="">
@@ -37,14 +37,13 @@
                </circle></svg><span>Reporting</span></div>
 </li>
 
- 
+
 
 
 <li class="menu @yield('manage_CDR_active') ">
     <a href="#Workers"  data-toggle="collapse" @yield('manage_CDR_aria') class="dropdown-toggle collapsed">
         <div class="">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-            <span>CDR Report</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call"><path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>  <span>CDR Report</span>
         </div>
         <div>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
@@ -52,8 +51,8 @@
     </a>
     <ul class="submenu list-unstyled collapse" id="Workers" data-parent="#accordionExample" style="">
 
-    
- 
+
+
 <li>
     <a  href="{{route('complain.view.incoming')}}" >Incoming Calls </a>
 </li>
@@ -68,5 +67,5 @@
 
 
 
- 
- 
+
+
